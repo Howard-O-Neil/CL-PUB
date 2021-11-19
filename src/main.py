6afd6bbe4a55beb2f40897e24be692d614b78411
mingkhoi <mingkhoitran1234@gmail.com>
2021-11-19 13:55:37 +0700

    polynomial; regularization; refactor source code

--- src/main.py
+++ src/main.py
@@ -1,38 +1,55 @@
 import tensorflow_core as tf
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
- 
-learning_rate = 0.01
-training_epochs = 40
 
-x_train = np.linspace(-1, 1, 100)
+import utils
 
-y_train_coeffs = [1, 2, 3, 4, 5, 6]
-y_train = 0
+learning_rate = 0.001
+training_epochs = 1000
+reg_lambda = 0.0
 
-for i in range(len(y_train_coeffs)):
-    y_train += y_train_coeffs[i] * np.power(x_train, i)
+x_dataset = np.linspace(-1, 1, 200)
 
-y_train += np.random.randn(*x_train.shape) * 1.5 # add noise
+num_coeffs = 9
+y_dataset_params = [0.0] * num_coeffs
+y_dataset_params[2] = 1
 
-# scatter the data
+y_dataset = 0
+for i in range(num_coeffs):
+    y_dataset += y_dataset_params[i] * np.power(x_dataset, i)
+y_dataset += np.random.randn(*x_dataset.shape) * 0.3
+(x_train, x_test, y_train, y_test) = utils.split_dataset(x_dataset, y_dataset, 0.7)
+
+# plot train data
 plt.scatter(x_train, y_train)
+plt.show()
+
+# plot test data
+plt.scatter(x_test, y_test)
 plt.show()
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
+
 def model(X, w):
     terms = []
-    for i in range(len(y_train_coeffs)):
+    for i in range(num_coeffs):
         term = tf.multiply(w[i], tf.pow(X, i))
         terms.append(term)
     return tf.add_n(terms)
 
-w = tf.Variable([0.] * len(y_train_coeffs), name="parameters")
+
+w = tf.Variable([0.0] * num_coeffs, name="parameters")
 y_model = model(X, w)
 
-cost = tf.pow(Y - y_model, 2)
+cost = tf.divide(
+    tf.add(
+        tf.reduce_sum(tf.square(Y - y_model)),
+        tf.multiply(reg_lambda, tf.reduce_sum(tf.square(w))),
+    ),
+    2 * x_train.size,
+)
 
 train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
@@ -40,18 +57,24 @@ sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 
-for epoch in range(training_epochs):
-    for (x, y) in zip(x_train, y_train):
-        sess.run(train_op, feed_dict={X: x, Y: y})
+list_w = list()
+list_final_cost = list()
 
-w_val = sess.run(w)
+for reg_lambda in np.linspace(0, 1, 100):
+    for epoch in range(training_epochs):
+        sess.run(train_op, feed_dict={X: x_train, Y: y_train})
+
+    print("## Before and after calculate cost")
+    print(np.sum(sess.run(w)))
+    list_final_cost.append(sess.run(cost, feed_dict={X: x_test, Y: y_test}))
+    list_w.append(sess.run(w))
+    print(np.sum(sess.run(w)))
+
+sess.run(cost, feed_dict={X: x_test, Y: y_test})
+print("## After go through all lambda")
+print(np.sum(sess.run(w)))
+
+
 sess.close()
 
-plt.scatter(x_train, y_train)
-
-y_learned = 0
-for i in range(len(y_train_coeffs)):
-    y_learned += w_val[i] * np.power(x_train, i)
-
-plt.plot(x_train, y_learned, "r")
-plt.show()
+print("===== Done training =====")
