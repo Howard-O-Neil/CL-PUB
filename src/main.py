1207b1935e4c1955834a959ab59ae1c366978cc0
mingkhoi <mingkhoitran1234@gmail.com>
2021-11-22 23:31:37 +0700

    linear regression

--- src/main.py
+++ src/main.py
@@ -1,26 +1,50 @@
 import tensorflow_core as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
-raw_data = np.random.normal(10, 1, 100)
+x_label0 = np.random.normal(5, 1, 10)
+x_label1 = np.random.normal(2, 1, 10)
+xs = np.append(x_label0, x_label1)
 
-alpha = tf.constant(0.05)
-curr_value = tf.placeholder(tf.float32)
-prev_avg = tf.Variable(0.)
+# just normal python array. not numpy
+temp = [0.0] * len(x_label0) + [1.0] * len(x_label1)
 
-update_avg = alpha * curr_value + (1 - alpha) * prev_avg
+labels = np.array(temp)
 
-avg_hist = tf.summary.scalar("running average", update_avg)
-value_hist = tf.summary.scalar("incoming_values", curr_value)
-merged = tf.summary.merge_all()
-writer = tf.summary.FileWriter("./logs")
+plt.scatter(xs, labels)
 
+learning_rate = 0.001
+training_epochs = 1000
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+
+def model(X, w):
+    return tf.add(tf.multiply(w[1], tf.pow(X, 1)), tf.multiply(w[0], tf.pow(X, 0)))
+
+
+w = tf.Variable([0.0, 0.0], name="parameters")
+y_model = model(X, w)
+
+cost = tf.reduce_sum(tf.square(Y - y_model))
+
+train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+sess = tf.Session()
 init = tf.global_variables_initializer()
+sess.run(init)
 
-with tf.Session() as sess:
-    sess.run(init)
-    for i in range(len(raw_data)):
-        summary_str, curr_avg = sess.run([merged, update_avg],
-            feed_dict={curr_value: raw_data[i]})
-        sess.run(tf.assign(prev_avg, curr_avg))
-        print(raw_data[i], curr_avg)
-        writer.add_summary(summary_str, i)
+for epoch in range(training_epochs):
+    sess.run(train_op, feed_dict={X: xs, Y: labels})
+    current_cost = sess.run(cost, feed_dict={X: xs, Y: labels})
+
+    if epoch % 100 == 0:
+        print(epoch, current_cost)
+
+w_val = sess.run(w)
+sess.close()
+
+plt.scatter(xs, labels)
+plt.plot(xs, xs * w_val[1] + w_val[0], 'r')
+plt.show()
